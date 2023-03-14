6f40f8bc8b36da8febf4699836607d8d661b8386
mkhodroo <mkhodroo@yahoo.com>
2023-03-14 12:43:08 +0330

    init

---
+++ resources/js/Components/NavLink.vue
@@ -0,0 +1,14 @@
+<script>
+import { computed } from 'vue';
+import { Head, Link } from '@inertiajs/vue3';
+
+export default{
+    name: 'Nav'
+}
+
+
+</script>
+
+<template dir="rtl">
+    <a :href="route('home')">صفحه نخست</a> |
+</template>
