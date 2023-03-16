242977b4584fafb5457ba837b32597e6b09daf45
mkhodroo <mkhodroo@yahoo.com>
2023-03-16 17:41:16 +0330

    init

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,24 @@
+<script>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Nav from '@/Components/NavLink.vue'
+
+export default {
+  name: 'Main',
+  components:{
+    Nav
+  }
+}
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+        </div>
+        <Nav></Nav>
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/mkhodroo/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+      <Main>
+        <h1>hell</h1>
+      </Main>
+    </div>
+</template>
+  
+<script>
+import Main from '@/Layouts/GuestLayout.vue'
+import VueRouter from 'vue-router'
+
+export default {
+name: 'App',
+components: {
+    Main
+}
+}
+</script>
