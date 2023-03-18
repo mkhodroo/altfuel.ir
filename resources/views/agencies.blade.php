73d1ee9ec745c9fd90dda96febd408a551b0d0d6
mkhodroo <mkhodroo@yahoo.com>
2023-03-18 14:06:26 +0330

    agencies

--- resources/views/agencies.blade.php
+++ resources/views/agencies.blade.php
@@ -1,16 +1,33 @@
 @extends('layout.main')
 
 @section('content')
-    
+    <table class="table table-striped" id="agencies">
+        <thead>
+            <tr>
+                <th>نام</th>
+                <th>استان</th>
+                <th>شهرستان</th>
+                <th>آدرس</th>
+                <th>تلفن</th>
+                <th>تاریخ انقضا پروانه کسب</th>
+            </tr>
+        </thead>
+    </table>
 @endsection
 
 @section('script')
     <script>
-        send_ajax_get_request(
-            'https://l.altfuel.ir/agencies/get',
-            function(data){
-                console.log(data);
-            }
+        create_datatable(
+            "agencies", 
+            'https://l.altfuel.ir/api/agencies/get',
+            [
+                { data: 'Name' },
+                { data: 'Province' },
+                { data: 'City' },
+                { data: 'Address' },
+                { data: 'Tel' },
+                { data: 'ExpDate' },
+            ]
         )
     </script>
 @endsection
